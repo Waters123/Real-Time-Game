0b5523f751eb5bdcb873e5ca9695cac4db800a02
Waters123 <guga33331@gmail.com>
2020-03-26 15:46:01 +0400

    added graphQL

--- server/index.js
+++ server/index.js
@@ -1,13 +1,66 @@
 const express = require("express");
-const http = require("http");
+const bodyParses = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const PORT = process.env.PORT || 5000;
 
-const router = require("./router");
-
 const app = express();
-const server = http.createServer(app);
 
-app.use(router);
+const events = [];
 
-server.listen(PORT, () => console.log(`server started on port ${PORT}`));
+app.use(bodyParses.json());
+
+app.use(
+  "/graphql",
+
+  graphqlHttp({
+    schema: buildSchema(`
+    type Event {
+        _id: ID!
+        name:String!
+        img:String!
+      
+    }
+
+    input EventInput {
+        name:String!
+        img:String!
+        
+    } 
+
+     type RootQuery{
+          events:[Event!]!
+
+     }
+
+     type RootMutation{
+          createEvent( eventInput:EventInput ):Event
+     }
+       schema {
+           query:RootQuery
+           mutation:RootMutation
+       }
+     `),
+
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: args => {
+        const event = {
+          _id: Math.random.toString(),
+          name: args.eventInput.name,
+          img: args.eventInput.img
+        };
+        console.log(args);
+        events.push(event);
+        return event;
+      }
+    },
+
+    graphiql: true
+  })
+);
+
+app.listen(PORT);
